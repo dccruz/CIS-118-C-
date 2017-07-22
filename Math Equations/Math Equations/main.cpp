3cb604f2533648be9f2b120b985c8aad41d3065f
DC Cruz <dccoder@icloud.com>
2017-07-22 13:27:57 -0700

    Math Equations

---
+++ Math Equations/Math Equations/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main( ) {
+cout << "DC Cruz July 8th 2017"<<endl;{
+    
+    double a(18);
+    int b=6, c=3;
+    
+cout<<a+b/c << endl;
+cout<<(a+b)/c << endl;
+cout<<a+(b/c) << endl;
+return 0;
+ }}
